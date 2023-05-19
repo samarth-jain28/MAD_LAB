6d52fbc760072bbf5b8c9a176e1ab43c473681a2
Samarth Jain <99988982+samarth-jain28@users.noreply.github.com>
2023-05-19 14:27:22 +0530

    final

---
+++ LAB1_prob2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class Main
+{
+    public static void main (String[] args)
+        {
+        	int i, j, n=5;
+	        for(i = n; i >= 1; i--)
+	            {
+		            for(j = i; j < n; j++)
+                		{
+                			System.out.print(" ");
+                		}
+            		for(j = 1; j <= (i - 1); j++)
+                		{
+                			System.out.print("* ");
+                		}
+	        System.out.println("");
+	           }
+        }
+}
